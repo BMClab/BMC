ecf82e140b67719f69a247bdd80940523bd1b1fa
Marcos Duarte <duartexyz@gmail.com>
2014-01-08 11:38:17 -0200

    update

--- functions/pdf_norm_plot.py
+++ functions/pdf_norm_plot.py
@@ -14,7 +14,7 @@ def pdf_norm_plot(m=0, s=1, fig=None, ax=None):
     m, s = float(m), float(s)  # in case they were passed as command line args
     if s <= 0:
         s = 1
-    plt.rc('font', size=18)
+    plt.rc('font', size=16)
     plt.rc(('xtick.major','ytick.major'), pad=8)
     n = 1000
     if fig is None:
@@ -26,11 +26,11 @@ def pdf_norm_plot(m=0, s=1, fig=None, ax=None):
     ones = np.arange(np.round(3/8*n)+1, np.round(5/8*n)+1, dtype=int)
     twos = np.arange(np.round(2/8*n)+1, np.round(6/8*n)+1, dtype=int)
     threes = np.arange(np.round(1/8*n)+1, np.round(7/8*n)+1, dtype=int)
-    ax.plot(x, f, color=[1, 0, 0, .8], linewidth=4)
-    ax.set_ylim(0, ax.get_ylim()[1]*1.1)
     ax.fill_between(x[ones], y1=f[ones], y2=0, color=[0, 0.2, .5, .4])
     ax.fill_between(x[twos], y1=f[twos], y2=0, color=[0, 0.2, .5, .3])
     ax.fill_between(x[threes], y1=f[threes], y2=0, color=[0, 0.2, .5, .3])
+    ax.plot(x, f, color=[1, 0, 0, .8], linewidth=4)
+    ax.set_ylim(0, ax.get_ylim()[1]*1.1)
     ymax = ax.get_ylim()[1]/0.45
     for i in range(-3, 4):
         ax.axvline(i*s+m, ymin=0, ymax=f[n/2-i/8*n]/ax.get_ylim()[1], c='w', lw=3)
@@ -42,19 +42,23 @@ def pdf_norm_plot(m=0, s=1, fig=None, ax=None):
     ax.text(3.2*s+m, .03*ymax, '0.1%', color='k')
     ax.text(-2.8*s+m, .08*ymax, '2.1%', color='k')
     ax.text(2.2*s+m, .08*ymax, '2.1%', color='k')
-    ax.text(-1.9*s+m, .03*ymax, '13.6%',  color='w')
-    ax.text(1.1*s+m, .03*ymax, '13.6%', color='w')
-    ax.text(-.9*s+m, .08*ymax, '34.1%', color='w')
-    ax.text(.1*s+m, .08*ymax, '34.1%', color='w')
-    ax.set_xlim(-1*s, 1*s)
+    ax.text(-1.85*s+m, .03*ymax, '13.6%',  color='w')
+    ax.text(1.15*s+m, .03*ymax, '13.6%', color='w')
+    ax.text(-.85*s+m, .08*ymax, '34.1%', color='w')
+    ax.text(.15*s+m, .08*ymax, '34.1%', color='w')
     plt.locator_params(axis = 'y', nbins = 5)
     ax.set_xticks(np.linspace(m-4*s, m+4*s, 9))
-    xtl = [r'-%d$\sigma$' %i for i in range(4, 0, -1)]
-    xtl.extend([r'$\mu$'])
-    xtl.extend([r'+%d$\sigma$' %i for i in range(1, 5, 1)])
+    xtl = [r'%+d$\sigma$' %i for i in range(-4, 5, 1)]
+    xtl[4] = r'$\mu$'
     ax.set_xticklabels(xtl)
-    title='Probability density function of the normal distribution'
-    plt.title(title, fontsize=16)
+    #ax2 = ax.twiny()
+    #ax2.xaxis.set_ticks_position('bottom')
+    #ax2.spines["bottom"].set_position(("axes", -0.1))
+    #ax2.set_xlim(-4*s, 4*s)
+    #ax2.set_xticks(np.linspace(m-4*s, m+4*s, 9))
+    title='Probability density function of the normal distribution ' +\
+    '~ $N(\mu=%.1f,\ \sigma^2=%.1f)$' %(m, s**2)
+    plt.title(title, fontsize=14)
     plt.show()
 
     return fig, ax
