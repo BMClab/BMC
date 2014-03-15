eb7c247ad43f5c9970fe7c71c4dc80203ef60d5c
Marcos Duarte <duartexyz@gmail.com>
2014-03-14 21:02:06 -0300

    update

---
+++ functions/invdyn2d.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+"""Euler rotation matrix given sequence, frame, and angles."""
+
+from __future__ import division, print_function
+
+__author__ = 'Marcos Duarte'
+__version__ = 'invdyn2d.py v.1 2014/20/02'
+
+
+def invdyn2d(rcm, rd, rp, acm, alpha, mass, Icm, Fd, Md):
+    """Two-dimensional inverse-dynamics calculations of one segment
+
+    Parameters
+    ----------
+    rcm   : array_like [x,y]
+            center of mass position (y is vertical)
+    rd    : array_like [x,y]
+            distal joint position
+    rp    : array_like [x,y]
+            proximal joint position
+    acm   : array_like [x,y]
+            center of mass acceleration
+    alpha : array_like [x,y]
+            segment angular acceleration
+    mass  : number
+            mass of the segment   
+    Icm   : number
+            rotational inertia around the center of mass of the segment
+    Fd    : array_like [x,y]
+            force on the distal joint of the segment
+    Md    : array_like [x,y]
+            moment of force on the distal joint of the segment
+    
+    Returns
+    -------
+    Fp    : array_like [x,y]
+            force on the proximal joint of the segment (y is vertical)
+    Mp    : array_like [x,y]
+            moment of force on the proximal joint of the segment
+
+    Notes
+    -----
+    To use this function recursevely, the outputs [Fp, Mp] must be inputed as 
+    [-Fp, -Mp] on the next call to represent [Fd, Md] on the distal joint of the
+    next segment (action-reaction).
+    
+    This code was inspired by a similar code written by Ton van den Bogert [1]_.
+    See this notebook [2]_.
+
+    References
+    ----------
+    .. [1] http://isbweb.org/data/invdyn/index.html
+    .. [2] http://nbviewer.ipython.org/github/duartexyz/BMC/blob/master/GaitAnalysis2D.ipynb
+
+    """
+    
+    from numpy import cross
+     
+    # Force and moment of force on the proximal joint
+    Fp = mass*acm - Fd - [0, -9.8*mass]
+    Mp = Icm*alpha - Md - cross(rd-rcm,Fd) - cross(rp-rcm,Fp)
+    
+    return Fp, Mp
