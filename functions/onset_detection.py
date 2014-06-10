64a671833321656ea4d43bd8cdb2f34cc3085aee
Marcos Duarte <duartexyz@gmail.com>
2014-06-10 15:21:28 -0300

    help

--- functions/onset_detection.py
+++ functions/onset_detection.py
@@ -9,7 +9,7 @@ __author__ = 'Marcos Duarte, https://github.com/duartexyz/BMC'
 __version__ = 'onset_detection.py v.2 2014/06/09'
 
 
-def onset_detection(data, threshold=0, n_above=1, n_below=0, show=False, ax=None):
+def onset_detection(x, threshold=0, n_above=1, n_below=0, show=False, ax=None):
     """Detects onset in data based on amplitude threshold.
 
     Parameters
@@ -17,13 +17,13 @@ def onset_detection(data, threshold=0, n_above=1, n_below=0, show=False, ax=None
     x : 1D array_like
         data.
     threshold : number, optional (default = 0)
-        minimum amplitude of the data to detect.
+        minimum amplitude of `x` to detect.
     n_above : number, optional (default = 1)
         minimum number of continuous samples greater than or equal to
-        threshold to detect (but see the parameter n_below).
+        `threshold` to detect (but see the parameter `n_below`).
     n_below : number, optional (default = 0)
-        minimum number of samples (continuous or not) below threshold
-        that will be ignored in the detection of data >= threshold.
+        minimum number of samples (continuous or not) below `threshold`
+        that will be ignored in the detection of `x` >= `threshold`.
     show  : bool, optional (default = True)
         True (1) plots data in matplotlib figure, False (0) don't plot.
     ax : a matplotlib.axes.Axes instance, optional (default = None).
@@ -59,13 +59,11 @@ def onset_detection(data, threshold=0, n_above=1, n_below=0, show=False, ax=None
     >>> onset_detection(x, threshold=1, n_above=1, n_below=0, show=True)
     """
 
-    data = np.atleast_1d(data).astype('float64')    
+    x = np.atleast_1d(x).astype('float64')    
     # deal with NaN's (by definition, NaN's are not greater than threshold)
-    data[np.isnan(data)] = -np.inf
-    if not data[np.isfinite(data)].size:
-        return np.array([])
+    x[np.isnan(x)] = -np.inf
     # indices of data greater than or equal to threshold
-    inds = np.nonzero(data >= threshold)[0]
+    inds = np.nonzero(x >= threshold)[0]
     if inds.size:
         # initial and final indexes of continuous data
         inds = np.vstack((inds[np.diff(np.hstack((-np.inf, inds))) > n_below+1], \
@@ -74,13 +72,13 @@ def onset_detection(data, threshold=0, n_above=1, n_below=0, show=False, ax=None
         inds = inds[inds[:, 1]-inds[:, 0] >= n_above-1, :]
     if not inds.size:
         inds = np.array([])  # standardize inds shape
-    if show and data.size > 1:  # don't waste my time ploting one datum
-        _plot(data, threshold, n_above, n_below, inds, ax)
+    if show and x.size > 1:  # don't waste my time ploting one datum
+        _plot(x, threshold, n_above, n_below, inds, ax)
 
     return inds
 
 
-def _plot(data, threshold, n_above, n_below, inds, ax):
+def _plot(x, threshold, n_above, n_below, inds, ax):
     """Plot results of the cogve function, see its help."""
     try:
         import matplotlib.pyplot as plt
@@ -93,21 +91,21 @@ def _plot(data, threshold, n_above, n_below, inds, ax):
         if inds.size:
             for (indi, indf) in inds:
                 if indi == indf:
-                    ax.plot(indf, data[indf], 'ro', mec='r', ms=6)
+                    ax.plot(indf, x[indf], 'ro', mec='r', ms=6)
                 else:
-                    ax.plot(range(indi, indf+1), data[indi:indf+1], 'r', lw=1)
+                    ax.plot(range(indi, indf+1), x[indi:indf+1], 'r', lw=1)
                     ax.axvline(x=indi, color='b', lw=1, ls='--')
                 ax.axvline(x=indf, color='b', lw=1, ls='--')
             inds = np.vstack((np.hstack((0, inds[:, 1])),
-                              np.hstack((inds[:, 0], data.size-1)))).T
+                              np.hstack((inds[:, 0], x.size-1)))).T
             for (indi, indf) in inds:
-                ax.plot(range(indi, indf+1), data[indi:indf+1], 'k', lw=1)
+                ax.plot(range(indi, indf+1), x[indi:indf+1], 'k', lw=1)
         else:
-            ax.plot(data, 'k', lw=1)
+            ax.plot(x, 'k', lw=1)
             ax.axhline(y=threshold, color='r', lw=1, ls='-')
 
-        ax.set_xlim(-.02*data.size, data.size*1.02-1)
-        ymin, ymax = data[np.isfinite(data)].min(), data[np.isfinite(data)].max()
+        ax.set_xlim(-.02*x.size, x.size*1.02-1)
+        ymin, ymax = x[np.isfinite(x)].min(), x[np.isfinite(x)].max()
         yrange = ymax - ymin if ymax > ymin else 1
         ax.set_ylim(ymin - 0.1*yrange, ymax + 0.1*yrange)
         ax.set_xlabel('Data #', fontsize=14)
