28cf33541b39db7ef99dbb192e17629a02c2505b
Marcos Duarte <duartexyz@gmail.com>
2023-08-01 04:59:57 -0300

    similarity

--- functions/similarity.py
+++ functions/similarity.py
@@ -7,13 +7,16 @@ __license__ = "MIT"
 
 import numpy as np
 
-def mse(y, axis1=0, axis2=1, central=np.nanmedian, normalization=np.nanmedian):
+
+def mse_(y: np.ndarray, axis1: int=0, axis2: int=1, central: callable=np.nanmedian,
+         normalization: callable=np.nanmedian) -> np.ndarray:
     """Mean Squared Error of `y` w.r.t. `central` across axis2 over axis1.
 
     Parameters
     ----------
     y : numpy array
-        array for the calculation of mse w.r.t. to a central statistics
+        an at least 2D array of data for the calculation of mean squared error
+        w.r.t. to a `central` statistics.
     axis1 : integer, optional (default = 0)
         axis to slice `y` ndarray in the calculation of mse.
     axis2 : integer, optional (default = 1)
@@ -38,7 +41,7 @@ def mse(y, axis1=0, axis2=1, central=np.nanmedian, normalization=np.nanmedian):
     >>> rng = np.random.default_rng()
     >>> y = rng.random((100, 10))
     >>> y +=  np.atleast_2d(np.sin(2*np.pi*np.linspace(0, 1, 100))).T
-    >>> mse(y, axis1=0, axis2=1, central=np.nanmedian, normalization=np.nanmedian)
+    >>> mse_(y, axis1=0, axis2=1, central=np.nanmedian, normalization=np.nanmedian)
 
     Version history
     ---------------
@@ -46,20 +49,22 @@ def mse(y, axis1=0, axis2=1, central=np.nanmedian, normalization=np.nanmedian):
         First release version
     """
 
-    import numpy as np
 
     score = np.empty((y.shape[axis2]))
     score.fill(np.nan)
     idx = np.where(~np.all(np.isnan(y), axis=axis1))[0]  # masked array is slow
     y = y.swapaxes(0, axis2)[idx, ...].swapaxes(0, axis2)  # faster than .take
-    score[idx] = np.nanmean((y - central(y, axis=axis2, keepdims=True))**2, axis=axis1)
+    score[idx] = np.nanmean((y - central(y, axis=axis2, keepdims=True))**2,
+                            axis=axis1)
     if normalization is not None:
         score = score/normalization(score)
     return score
 
 
-def similarity(y, axis1=0, axis2=1, threshold=0, nmin=3,
-               recursive=True, metric=mse, msg=True, **kwargs):
+def similarity(y: np.ndarray, axis1: int=0, axis2: int=1, threshold: float=0,
+               nmin: int=3, recursive: bool=True, metric: callable=mse_,
+               msg: bool=True, **kwargs: callable) -> tuple[np.ndarray, np.ndarray,
+                                                            np.ndarray, np.ndarray]:
     """Select data vectors by similarity using a metric score.
 
     Parameters
@@ -73,17 +78,29 @@ def similarity(y, axis1=0, axis2=1, threshold=0, nmin=3,
     threshold : float, optional (default = 0)
         If greater than 0, vector with mse above it will be discarded.
         If 0, threshold will be automatically calculated as the
-        minimum of [pct[1] + 1.5*(pct[2]-pct[0]), score[-2], 3], where
-        score[-2] is the before largest mse value among the vectors
-        calculated at the first time (not updated by `recursive` option.
-    nmin : interger, optional (default = 3)
+        minimum of [q[1] + 1.5*(q[2]-q[0]), score[-2], 3], where q's are the
+        quantiles and score[-2] is the before-last largest score of `metric`
+        among vectors calculated at the first time, not updated by `recursive`
+        option.
+    nmin : integer, optional (default = 3)
         If greater than 0, minumum number of vectors to keep.
         If lower than 0, maximum number of vectors to discard.
     recursive :bool, optional (default = True)
-        Whether to calculate similarity metric recursevely. With the
-        recursive option, the mse values are computed again each time a
-        vector is discarded.
-    metric :
+        Whether to calculate similarity `metric` recursevely, updating the score
+        calculation each time a vector is discarded.
+        With `recursive` True, the output `score_all` will contain at each row
+        the updated score values for the used `metric` for each data vector.
+        The first row will contain the calculated original scores before any
+        vector was discarded. On the subsequent rows, the vector discarded is
+        represented by a NaN value and the kept vectors by their updated scores.
+        The last row will contain the updated scores of the final vectors kept.
+        With the `recursive` False, the comparison of score values with `threshold`
+        are made only once and vectors are discarded accordingly at once.
+        In this case, the output `score_all` will contain only two rows, the
+        first row will contain the calculated original scores before any vectors
+        were discarded. At the second row, the vectors discarded are represented
+        with NaN values and the kept vectors by their updated scores.
+    metric : optional (default=mse_)
         Function to use as metric to compute similarity.
     msg : bool, optional (default = True)
         Whether to print some messages.
@@ -132,53 +149,56 @@ def similarity(y, axis1=0, axis2=1, threshold=0, nmin=3,
         First release version
     """
 
-    import numpy as np
 
     if y.ndim < 2:
-        raise Exception('The input array must be at least a 2-D array.')
+        raise ValueError('The input array must be at least a 2-D array.')
     y = y.copy()
     score = metric(y, axis1=axis1, axis2=axis2, **kwargs)
     score_all = np.atleast_2d(score)
     ikept = np.where(~np.isnan(score))[0]  # indexes of kept vectors
-    inotkept = np.where(np.isnan(score))[0]  # indexes of not kept (discarded) vectors
+    # indexes of not kept (discarded) vectors
+    inotkept = np.where(np.isnan(score))[0]
     idx = np.argsort(score)
     score = score[idx]
-    n = np.count_nonzero(~np.isnan(score))  # number of kept vectors
-    if n < 3:
-        raise Exception('The input array must have at least 3 valid vectors.')
+    nkept = np.count_nonzero(~np.isnan(score))  # number of kept vectors
+    if nkept < 3:
+        raise ValueError('The input array must have at least 3 valid vectors.')
     if nmin < 0:
-        nmin = np.max([3, n + nmin])
+        nmin = np.max([3, nkept + nmin])
     if threshold == 0:
-        pct = np.nanpercentile(score, [25, 50, 75])
-        threshold = np.min([pct[1] + 1.5*(pct[2]-pct[0]), score[-2], 3])
+        q = np.nanquantile(a=score, q=[.25, .50, .75])
+        threshold = np.min([q[1] + 1.5*(q[2]-q[0]), score[-2], 3])
         if msg:
             print(f'Calculated threshold: {threshold}')
     if not recursive:  # discard all vectors at once
         idx2 = np.nonzero(score > threshold)[0]  # vectors to discard
         if len(idx2) > 0:
-            if n > nmin:  # keep at least nmin vectors
+            if nkept > nmin:  # keep at least nmin vectors
                 inotkept = np.r_[inotkept, idx[idx2[-(y.shape[axis2] - nmin):]][::-1]]
                 y.swapaxes(0, axis2)[inotkept, ...] = np.nan
                 score = metric(y, axis1=axis1, axis2=axis2, **kwargs)
                 score_all = np.vstack((score_all, score))
             elif msg:
-                print(f'Number of vectors to discard is greater than number to keep ({n}).')
+                print(
+                    f'Number of vectors to discard is greater than number to keep ({nkept}).')
     else:  # discard vectors with largest updated score one by one
-        while n > nmin and score[n-1] > threshold:
-            inotkept = np.r_[inotkept, idx[n-1]]
+        while nkept > nmin and score[nkept-1] > threshold:
+            inotkept = np.r_[inotkept, idx[nkept-1]]
             y.swapaxes(0, axis2)[inotkept[-1], ...] = np.nan
             score = metric(y, axis1=axis1, axis2=axis2, **kwargs)
             score_all = np.vstack((score_all, score))
             idx = np.argsort(score)
             score = score[idx]
-            n = n - 1
-        if msg and n == nmin and score[n-1] > threshold:
-            print(f'Number of vectors to discard is greater than number to keep ({n}).')
+            nkept = nkept - 1
+        if msg and nkept == nmin and score[nkept-1] > threshold:
+            print(
+                f'Number of vectors to discard is greater than number to keep ({nkept}).')
 
     if len(inotkept):
         ikept = np.setdiff1d(ikept, inotkept)
         y = y.swapaxes(0, axis2)[ikept, ...].swapaxes(0, axis2)
         if msg:
-            print(f'Data vectors discarded (in dimension {axis2}, n={len(inotkept)}): {inotkept}')
+            print(
+                f'Vectors discarded (in dimension {axis2}, n={len(inotkept)}): {inotkept}')
 
     return y, ikept, inotkept, score_all
