24a1a0e878e1c31dc0c659ebd2db526391303ab3
Marcos Duarte <duartexyz@gmail.com>
2023-08-04 12:08:26 -0300

    similarity

--- functions/simila.py
+++ functions/simila.py
@@ -1,16 +1,26 @@
 """Select data vectors by similarity using a metric score.
 """
 
+from typing import Callable
+import numpy as np
+import logging
+
 __author__ = 'Marcos Duarte, https://github.com/demotu/BMC'
 __version__ = 'simila.py v.1.0.0 20123/07/31'
 __license__ = "MIT"
 
-import numpy as np
+# set logger and configure it not to confuse with matplotlib debug messages
+logger = logging.getLogger(__name__)
+logger.setLevel(20)  # 0: NOTSET, 10: DEBUG, 20: INFO, 30: WARNING, 40: ERROR, 50: CRITICAL
+ch = logging.StreamHandler()
+formatter = logging.Formatter('{name}: {levelname}: {message}', style='{')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 
-def mse(y: np.ndarray, axis1: int=0, axis2: int=1, central: callable=np.nanmedian,
-         normalization: callable=np.nanmedian
-         ) -> np.ndarray:
+def mse(y: np.ndarray, axis1: int = 0, axis2: int = 1, central: Callable = np.nanmedian,
+        normalization: Callable = np.nanmedian
+        ) -> np.ndarray:
     """Mean Squared Error of `y` w.r.t. `central` across axis2 over axis1.
 
     Parameters
@@ -50,22 +60,24 @@ def mse(y: np.ndarray, axis1: int=0, axis2: int=1, central: callable=np.nanmedia
         First release version
     """
 
+    logger.debug('mse...')
 
     score = np.empty((y.shape[axis2]))
     score.fill(np.nan)
     idx = np.where(~np.all(np.isnan(y), axis=axis1))[0]  # masked array is slow
     y = y.swapaxes(0, axis2)[idx, ...].swapaxes(0, axis2)  # faster than .take
-    score[idx] = np.nanmean((y - central(y, axis=axis2, keepdims=True))**2,
-                            axis=axis1)
+    score[idx] = np.nanmean((y - central(y, axis=axis2, keepdims=True))**2, axis=axis1)
     if normalization is not None:
         score = score/normalization(score)
+    logger.debug(f'idx: {idx}, score: {score}')
+
     return score
 
 
-def similarity(y: np.ndarray, axis1: int=0, axis2: int=1, threshold: float=0,
-               nmin: int=3, recursive: bool=True, metric: callable=mse,
-               drop=False, msg: bool=True, **kwargs: callable
-               ) ->tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+def similarity(y: np.ndarray, axis1: int = 0, axis2: int = 1, threshold: float = 0,
+               nmin: int = 3, repeat: bool = True, metric: Callable = mse,
+               drop=True, msg: bool = True, **kwargs: Callable
+               ) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
 
     """Select data vectors by similarity using a metric score.
 
@@ -82,29 +94,31 @@ def similarity(y: np.ndarray, axis1: int=0, axis2: int=1, threshold: float=0,
         If 0, threshold will be automatically calculated as the
         minimum of [q[1] + 1.5*(q[2]-q[0]), score[-2], 3], where q's are the
         quantiles and score[-2] is the before-last largest score of `metric`
-        among vectors calculated at the first time, not updated by `recursive`
+        among vectors calculated at the first time, not updated by `repeat`
         option.
     nmin : integer, optional (default = 3)
         If greater than 0, minumum number of vectors to keep.
         If lower than 0, maximum number of vectors to discard.
-    recursive :bool, optional (default = True)
-        Whether to calculate similarity `metric` recursevely, updating the score
-        calculation each time a vector is discarded.
-        With `recursive` True, the output `scores` will contain at each row
+    repeat :bool, optional (default = True)
+        Whether to calculate similarity `metric` repeatedly, updating the
+        score calculation each time a vector is discarded.
+        With `repeat` True, the output `scores` will contain at each row
         the updated score values for the used `metric` for each data vector.
         The first row will contain the calculated original scores before any
-        vector was discarded. On the subsequent rows, the vector discarded is
-        represented by a NaN value and the kept vectors by their updated scores.
+        vector was discarded. On the next equent rows, the vectors discarded
+        are represented by NaN values and the kept vectors by their updated
+        scores.
         The last row will contain the updated scores of the final vectors kept.
-        With the `recursive` False, the comparison of score values with `threshold`
-        are made only once and vectors are discarded accordingly at once.
-        In this case, the output `scores` will contain only two rows, the
-        first row will contain the calculated original scores before any vectors
-        were discarded. At the second row, the vectors discarded are represented
-        with NaN values and the kept vectors by their updated scores.
+        With the `repeat` False, the comparison of score values with
+        `threshold` are made only once and vectors are discarded accordingly at
+        once. In this case, the output `scores` will contain only two rows, the
+        first row will contain the calculated original scores before any
+        vectors were discarded. At the second row, the vectors discarded are
+        represented with NaN values and the kept vectors by their updated
+        scores.
     metric : optional (default=mse)
         Function to use as metric to compute similarity.
-    drop : bool, optional (default = False)
+    drop : bool, optional (default = True)
         Whether to drop (delete) the discarded vectors from `y` in the output.
         If False, the values of the vectors discarded will be replaced by nans
         and the returned array will have the same dimensions as the original
@@ -138,7 +152,7 @@ def similarity(y: np.ndarray, axis1: int=0, axis2: int=1, threshold: float=0,
     >>> import matplotlib.pyplot as plt
     >>> rng = np.random.default_rng()
     >>> t, n = 100, 10
-    >>> y = rng.random((t, n))
+    >>> y = rng.random((t, n)) / 2
     >>> y +=  np.atleast_2d(2*np.sin(2*np.pi*np.linspace(0, 1, t))).T
     >>> for i in range(0, n, 2):
     >>>    j = rng.integers(t-20)
@@ -159,6 +173,7 @@ def similarity(y: np.ndarray, axis1: int=0, axis2: int=1, threshold: float=0,
         First release version
     """
 
+    logger.debug('Similarity...')
 
     if y.ndim < 2:
         raise ValueError('The input array must be at least a 2-D array.')
@@ -166,29 +181,30 @@ def similarity(y: np.ndarray, axis1: int=0, axis2: int=1, threshold: float=0,
     score = metric(y, axis1=axis1, axis2=axis2, **kwargs)
     scores = np.atleast_2d(score)
     ikept = np.where(~np.isnan(score))[0]  # indexes of kept vectors
-    # indexes of not kept (discarded) vectors
-    inotkept = np.where(np.isnan(score))[0]
+    inotkept = np.where(np.isnan(score))[0]  # indexes of discarded vectors
     idx = np.argsort(score)
     score = score[idx]
     nkept = np.count_nonzero(~np.isnan(score))  # number of kept vectors
     if nkept < 3:
+        logger.debug(f'nkept: {nkept}')
         raise ValueError('The input array must have at least 3 valid vectors.')
     if nmin < 0:
         nmin = np.max([3, nkept + nmin])
     if threshold == 0:
         q = np.nanquantile(a=score, q=[.25, .50, .75])
-        threshold = np.min([q[1] + 1.5*(q[2]-q[0]), score[-2], 3])
+        threshold = np.min([q[1] + 1.5*(q[2]-q[0]), score[-2], 3.])
         if msg:
             print(f'Calculated threshold: {threshold}')
-    if not recursive:  # discard all vectors at once
+    if not repeat:  # discard all vectors at once
         idx2 = np.nonzero(score > threshold)[0]  # vectors to discard
         if len(idx2) > 0:
             if nkept > nmin:  # keep at least nmin vectors
                 inotkept = np.r_[inotkept, idx[idx2[-(y.shape[axis2] - nmin):]][::-1]]
                 y.swapaxes(0, axis2)[inotkept, ...] = np.nan
                 score = metric(y, axis1=axis1, axis2=axis2, **kwargs)
+                logger.debug(f'not repeat - score: {score}')
                 scores = np.vstack((scores, score))
-    else:  # discard vectors with largest updated score one by one
+    else:  # discard vector with largest updated score one by one
         while nkept > nmin and score[nkept-1] > threshold:
             inotkept = np.r_[inotkept, idx[nkept-1]]
             y.swapaxes(0, axis2)[inotkept[-1], ...] = np.nan
@@ -197,6 +213,7 @@ def similarity(y: np.ndarray, axis1: int=0, axis2: int=1, threshold: float=0,
             idx = np.argsort(score)
             score = score[idx]
             nkept = nkept - 1
+            logger.debug(f'repeat - nkept: {nkept}, score: {score}')
 
     if len(inotkept):
         ikept = np.setdiff1d(ikept, inotkept)
@@ -204,6 +221,6 @@ def similarity(y: np.ndarray, axis1: int=0, axis2: int=1, threshold: float=0,
             y = y.swapaxes(0, axis2)[ikept, ...].swapaxes(0, axis2)
         if msg:
             print(
-                f'Vectors discarded (in dimension {axis2}, n={len(inotkept)}): {inotkept}')
+                  f'Vectors discarded (dimension {axis2}, n={len(inotkept)}): {inotkept}')
 
     return y, ikept, inotkept, scores
